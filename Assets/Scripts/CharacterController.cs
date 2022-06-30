1fc6189e2425f7316c63e87299a71dd415654ccf
tonydeta <81664572+shinichiost@users.noreply.github.com>
2022-06-30 17:36:37 +0700

    lam ca buoi chieu

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,25 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class CharacterController : MonoBehaviour
 {
     public static CharacterController instance { get; private set; }
-    public List<GameObject> gameoj;
+    [Header("Character :")]
+    public List<GameObject> gameoj = new List<GameObject>();
+    private GameObject oj;
+    private int index ;
+    public Button btnleft, btnright;
     private void Awake()
     {
+        
         instance = this;
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        index = 0;
+        oj = character();
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        oj.SetActive(true);
+        btnleft.onClick.AddListener(decreaseIndex);
+        btnright.onClick.AddListener(increaseIndex);
+
+    }
+    void decreaseIndex()
+    {
+        oj.SetActive(false);
+        if(index > 0)
+            index -= 1;
+        else
+            index = gameoj.Count - 1;
+        oj = character();
+        oj.SetActive(true);
+    }
+    void increaseIndex()
+    {
+        oj.SetActive(false);
+        if (index < gameoj.Count - 1)
+            index += 1;
+        else
+            index = 0;
+        oj = character();
+        oj.SetActive(true);
+    }
+    private GameObject character()
+    {
+        return gameoj[index];
     }
 }
--- Assets/btnplayonclick.cs
+++ Assets/btnplayonclick.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainController : MonoBehaviour
+public class btnplayonclick : MonoBehaviour
 {
-    private GameObject character;
     // Start is called before the first frame update
     void Start()
     {
-        character = CharacterController.instance.gameObject;
+        
     }
 
     // Update is called once per frame
